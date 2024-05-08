9c4171bb4df5563f51a25c24933e2dbfa4b94191
skraft-redhat <skraft@redhat.com>
2024-05-08 19:32:03 +0200

    Adding greeting

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -5,12 +5,15 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Hello!"
+        + "\this is version 1.0"
+        + "\tI'm running in pod " + System.getenv("HOSTNAME")
+        + "\tI'm reachable via " +System.getenv("KUBERNETES_SERVICE_HOST");
     }
 }
